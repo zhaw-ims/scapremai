eb85467f6cab43cb26a6d0ea2e7eea76824a68cd
Marko Ristin <marko.ristin@gmail.com>
2020-04-03 15:06:17 +0200

    Move C++ mapried to source root
    This commit moves C++ files generated by Mapry to source root in order to make it easier to integrate various linting and formatting tools (*e.g.*, clang-tidy and clang-format).

--- src/cpp/mapried/sampling/jsoncpp.cpp
+++ src/cpp/mapried/sampling/jsoncpp.cpp
@@ -11,7 +11,6 @@
 #include <string>
 #include <utility>
 
-namespace scapremai {
 namespace mapried {
 namespace sampling {
 
@@ -41,31 +40,27 @@ std::string message(const char* cc, size_t cc_size, std::string s) {
  */
 std::string value_type_to_string(Json::ValueType value_type) {
   switch (value_type) {
-    case Json::ValueType::nullValue:
-      return "null";
-    case Json::ValueType::intValue:
-      return "int";
-    case Json::ValueType::uintValue:
-      return "uint";
-    case Json::ValueType::realValue:
-      return "real";
-    case Json::ValueType::stringValue:
-      return "string";
-    case Json::ValueType::booleanValue:
-      return "bool";
-    case Json::ValueType::arrayValue:
-      return "array";
-    case Json::ValueType::objectValue:
-      return "object";
+    case Json::ValueType::nullValue: return "null";
+    case Json::ValueType::intValue: return "int";
+    case Json::ValueType::uintValue: return "uint";
+    case Json::ValueType::realValue: return "real";
+    case Json::ValueType::stringValue: return "string";
+    case Json::ValueType::booleanValue: return "bool";
+    case Json::ValueType::arrayValue: return "array";
+    case Json::ValueType::objectValue: return "object";
     default:
       std::stringstream ss;
-      ss << "Unhandled value type in value_to_string: " << value_type;
+      ss << "Unhandled value type in value_to_string: "
+        << value_type;
       throw std::domain_error(ss.str());
   }
 }
 
-void sampling_from(const Json::Value& value, std::string ref, Sampling* target,
-                   parse::Errors* errors) {
+void sampling_from(
+    const Json::Value& value,
+    std::string ref,
+    Sampling* target,
+    parse::Errors* errors) {
   if (errors == nullptr) {
     throw std::invalid_argument("Unexpected null errors");
   }
@@ -75,10 +70,16 @@ void sampling_from(const Json::Value& value, std::string ref, Sampling* target,
   }
 
   if (!value.isObject()) {
-    constexpr auto expected_but_got("Expected an object, but got: ");
+    constexpr auto expected_but_got(
+      "Expected an object, but got: ");
 
-    errors->add(ref, message(expected_but_got, strlen(expected_but_got),
-                             value_type_to_string(value.type())));
+    errors->add(
+      ref,
+      message(
+        expected_but_got,
+        strlen(expected_but_got),
+        value_type_to_string(
+          value.type())));
     return;
   }
 
@@ -94,16 +95,23 @@ void sampling_from(const Json::Value& value, std::string ref, Sampling* target,
   if (value.isMember("inputs")) {
     const Json::Value& obj = value["inputs"];
     if (!obj.isObject()) {
-      constexpr auto expected_but_got("Expected an object, but got: ");
+      constexpr auto expected_but_got(
+        "Expected an object, but got: ");
 
-      errors->add(inputs_ref,
-                  message(expected_but_got, strlen(expected_but_got),
-                          value_type_to_string(obj.type())));
+      errors->add(
+        inputs_ref,
+        message(
+          expected_but_got,
+          strlen(expected_but_got),
+          value_type_to_string(
+            obj.type())));
     } else {
-      for (Json::ValueConstIterator it = obj.begin(); it != obj.end(); ++it) {
-        auto instance = std::make_unique<Input>();
+      for (Json::ValueConstIterator it = obj.begin();
+          it != obj.end(); ++it) {
+                auto instance = std::make_unique<Input>();
         instance->id = it.name();
         target->inputs[it.name()] = std::move(instance);
+
       }
     }
   }
@@ -132,12 +140,20 @@ void sampling_from(const Json::Value& value, std::string ref, Sampling* target,
     const Json::Value& obj = value["inputs"];
 
     for (Json::ValueConstIterator it = obj.begin(); it != obj.end(); ++it) {
-      instance_ref.reserve(inputs_ref.size() + 1 + it.name().size());
-      instance_ref.resize(inputs_ref.size() + 1);
-      instance_ref.append(it.name());
+      instance_ref.reserve(
+        inputs_ref.size() + 1 + it.name().size());
+      instance_ref.resize(
+        inputs_ref.size() + 1);
+      instance_ref.append(
+        it.name());
 
-      Input* instance(target->inputs.at(it.name()).get());
-      input_from(*it, instance_ref, instance, errors);
+      Input* instance(
+        target->inputs.at(it.name()).get());
+      input_from(
+        *it,
+        instance_ref,
+        instance,
+        errors);
 
       if (errors->full()) {
         break;
@@ -153,11 +169,17 @@ void sampling_from(const Json::Value& value, std::string ref, Sampling* target,
   ////
 
   if (!value.isMember("output")) {
-    errors->add(ref, "Property is missing: output");
+    errors->add(
+      ref,
+      "Property is missing: output");
   } else {
     const Json::Value& value_0 = value["output"];
-    output_from(value_0, std::string(ref).append("/output"), &target->output,
-                errors);
+    output_from(
+      value_0,
+      std::string(ref)
+        .append("/output"),
+      &target->output,
+      errors);
   }
   if (errors->full()) {
     return;
@@ -168,28 +190,40 @@ void sampling_from(const Json::Value& value, std::string ref, Sampling* target,
   ////
 
   if (!value.isMember("bins")) {
-    errors->add(ref, "Property is missing: bins");
+    errors->add(
+      ref,
+      "Property is missing: bins");
   } else {
     const Json::Value& value_1 = value["bins"];
     if (!value_1.isInt64()) {
-      constexpr auto expected_but_got("Expected an int64, but got: ");
+      constexpr auto expected_but_got(
+        "Expected an int64, but got: ");
 
-      errors->add(std::string(ref).append("/bins"),
-                  message(expected_but_got, strlen(expected_but_got),
-                          value_type_to_string(value_1.type())));
+      errors->add(
+        std::string(ref)
+          .append("/bins"),
+        message(
+          expected_but_got,
+          strlen(expected_but_got),
+          value_type_to_string(
+            value_1.type())));
     } else {
       const auto cast_1 = value_1.asInt64();
       bool ok_1 = true;
 
       if (!(cast_1 >= 1)) {
         constexpr auto expected_but_got(
-            "Expected "
-            ">= 1"
-            ", but got: ");
+          "Expected "
+          ">= 1"
+          ", but got: ");
 
-        errors->add(std::string(ref).append("/bins"),
-                    message(expected_but_got, strlen(expected_but_got),
-                            std::to_string(cast_1)));
+        errors->add(
+          std::string(ref)
+            .append("/bins"),
+          message(
+            expected_but_got,
+            strlen(expected_but_got),
+            std::to_string(cast_1)));
         ok_1 = false;
       }
 
@@ -203,13 +237,22 @@ void sampling_from(const Json::Value& value, std::string ref, Sampling* target,
   }
 }
 
-void input_from(const Json::Value& value, std::string ref, Input* target,
-                parse::Errors* errors) {
+void input_from(
+    const Json::Value& value,
+    std::string ref,
+    Input* target,
+    parse::Errors* errors) {
   if (!value.isObject()) {
-    constexpr auto expected_but_got("Expected an object, but got: ");
+    constexpr auto expected_but_got(
+      "Expected an object, but got: ");
 
-    errors->add(ref, message(expected_but_got, strlen(expected_but_got),
-                             value_type_to_string(value.type())));
+    errors->add(
+      ref,
+      message(
+        expected_but_got,
+        strlen(expected_but_got),
+        value_type_to_string(
+          value.type())));
     return;
   }
 
@@ -218,15 +261,23 @@ void input_from(const Json::Value& value, std::string ref, Input* target,
   ////
 
   if (!value.isMember("identifier")) {
-    errors->add(ref, "Property is missing: identifier");
+    errors->add(
+      ref,
+      "Property is missing: identifier");
   } else {
     const Json::Value& value_0 = value["identifier"];
     if (!value_0.isString()) {
-      constexpr auto expected_but_got("Expected a string, but got: ");
+      constexpr auto expected_but_got(
+        "Expected a string, but got: ");
 
-      errors->add(std::string(ref).append("/identifier"),
-                  message(expected_but_got, strlen(expected_but_got),
-                          value_type_to_string(value_0.type())));
+      errors->add(
+        std::string(ref)
+          .append("/identifier"),
+        message(
+          expected_but_got,
+          strlen(expected_but_got),
+          value_type_to_string(
+            value_0.type())));
     } else {
       target->identifier = value_0.asString();
     }
@@ -240,15 +291,23 @@ void input_from(const Json::Value& value, std::string ref, Input* target,
   ////
 
   if (!value.isMember("range_begin")) {
-    errors->add(ref, "Property is missing: range_begin");
+    errors->add(
+      ref,
+      "Property is missing: range_begin");
   } else {
     const Json::Value& value_1 = value["range_begin"];
     if (!value_1.isDouble()) {
-      constexpr auto expected_but_got("Expected a double, but got: ");
+      constexpr auto expected_but_got(
+        "Expected a double, but got: ");
 
-      errors->add(std::string(ref).append("/range_begin"),
-                  message(expected_but_got, strlen(expected_but_got),
-                          value_type_to_string(value_1.type())));
+      errors->add(
+        std::string(ref)
+          .append("/range_begin"),
+        message(
+          expected_but_got,
+          strlen(expected_but_got),
+          value_type_to_string(
+            value_1.type())));
     } else {
       target->range_begin = value_1.asDouble();
     }
@@ -262,15 +321,23 @@ void input_from(const Json::Value& value, std::string ref, Input* target,
   ////
 
   if (!value.isMember("range_end")) {
-    errors->add(ref, "Property is missing: range_end");
+    errors->add(
+      ref,
+      "Property is missing: range_end");
   } else {
     const Json::Value& value_2 = value["range_end"];
     if (!value_2.isDouble()) {
-      constexpr auto expected_but_got("Expected a double, but got: ");
+      constexpr auto expected_but_got(
+        "Expected a double, but got: ");
 
-      errors->add(std::string(ref).append("/range_end"),
-                  message(expected_but_got, strlen(expected_but_got),
-                          value_type_to_string(value_2.type())));
+      errors->add(
+        std::string(ref)
+          .append("/range_end"),
+        message(
+          expected_but_got,
+          strlen(expected_but_got),
+          value_type_to_string(
+            value_2.type())));
     } else {
       target->range_end = value_2.asDouble();
     }
@@ -280,13 +347,22 @@ void input_from(const Json::Value& value, std::string ref, Input* target,
   }
 }
 
-void output_from(const Json::Value& value, std::string ref, Output* target,
-                 parse::Errors* errors) {
+void output_from(
+    const Json::Value& value,
+    std::string ref,
+    Output* target,
+    parse::Errors* errors) {
   if (!value.isObject()) {
-    constexpr auto expected_but_got("Expected an object, but got: ");
+    constexpr auto expected_but_got(
+      "Expected an object, but got: ");
 
-    errors->add(ref, message(expected_but_got, strlen(expected_but_got),
-                             value_type_to_string(value.type())));
+    errors->add(
+      ref,
+      message(
+        expected_but_got,
+        strlen(expected_but_got),
+        value_type_to_string(
+          value.type())));
     return;
   }
 
@@ -295,15 +371,23 @@ void output_from(const Json::Value& value, std::string ref, Output* target,
   ////
 
   if (!value.isMember("identifier")) {
-    errors->add(ref, "Property is missing: identifier");
+    errors->add(
+      ref,
+      "Property is missing: identifier");
   } else {
     const Json::Value& value_0 = value["identifier"];
     if (!value_0.isString()) {
-      constexpr auto expected_but_got("Expected a string, but got: ");
+      constexpr auto expected_but_got(
+        "Expected a string, but got: ");
 
-      errors->add(std::string(ref).append("/identifier"),
-                  message(expected_but_got, strlen(expected_but_got),
-                          value_type_to_string(value_0.type())));
+      errors->add(
+        std::string(ref)
+          .append("/identifier"),
+        message(
+          expected_but_got,
+          strlen(expected_but_got),
+          value_type_to_string(
+            value_0.type())));
     } else {
       target->identifier = value_0.asString();
     }
@@ -317,15 +401,23 @@ void output_from(const Json::Value& value, std::string ref, Output* target,
   ////
 
   if (!value.isMember("range_begin")) {
-    errors->add(ref, "Property is missing: range_begin");
+    errors->add(
+      ref,
+      "Property is missing: range_begin");
   } else {
     const Json::Value& value_1 = value["range_begin"];
     if (!value_1.isDouble()) {
-      constexpr auto expected_but_got("Expected a double, but got: ");
+      constexpr auto expected_but_got(
+        "Expected a double, but got: ");
 
-      errors->add(std::string(ref).append("/range_begin"),
-                  message(expected_but_got, strlen(expected_but_got),
-                          value_type_to_string(value_1.type())));
+      errors->add(
+        std::string(ref)
+          .append("/range_begin"),
+        message(
+          expected_but_got,
+          strlen(expected_but_got),
+          value_type_to_string(
+            value_1.type())));
     } else {
       target->range_begin = value_1.asDouble();
     }
@@ -339,15 +431,23 @@ void output_from(const Json::Value& value, std::string ref, Output* target,
   ////
 
   if (!value.isMember("range_end")) {
-    errors->add(ref, "Property is missing: range_end");
+    errors->add(
+      ref,
+      "Property is missing: range_end");
   } else {
     const Json::Value& value_2 = value["range_end"];
     if (!value_2.isDouble()) {
-      constexpr auto expected_but_got("Expected a double, but got: ");
+      constexpr auto expected_but_got(
+        "Expected a double, but got: ");
 
-      errors->add(std::string(ref).append("/range_end"),
-                  message(expected_but_got, strlen(expected_but_got),
-                          value_type_to_string(value_2.type())));
+      errors->add(
+        std::string(ref)
+          .append("/range_end"),
+        message(
+          expected_but_got,
+          strlen(expected_but_got),
+          value_type_to_string(
+            value_2.type())));
     } else {
       target->range_end = value_2.asDouble();
     }
@@ -357,7 +457,8 @@ void output_from(const Json::Value& value, std::string ref, Output* target,
   }
 }
 
-Json::Value serialize_input(const Input& input) {
+Json::Value serialize_input(
+    const Input& input) {
   Json::Value input_as_value;
 
   input_as_value["identifier"] = input.identifier;
@@ -369,7 +470,8 @@ Json::Value serialize_input(const Input& input) {
   return input_as_value;
 }
 
-Json::Value serialize_output(const Output& output) {
+Json::Value serialize_output(
+    const Output& output) {
   Json::Value output_as_value;
 
   output_as_value["identifier"] = output.identifier;
@@ -381,7 +483,8 @@ Json::Value serialize_output(const Output& output) {
   return output_as_value;
 }
 
-Json::Value serialize_sampling(const Sampling& sampling) {
+Json::Value serialize_sampling(
+    const Sampling& sampling) {
   Json::Value sampling_as_value;
 
   sampling_as_value["output"] = serialize_output(sampling.output);
@@ -396,14 +499,15 @@ Json::Value serialize_sampling(const Sampling& sampling) {
 
       if (id != instance->id) {
         constexpr auto expected(
-            "Expected the class instance of "
-            "Input"
-            "to have the ID ");
+          "Expected the class instance of "
+          "Input"
+          "to have the ID ");
         constexpr auto but_got(", but got: ");
 
         std::string msg;
-        msg.reserve(strlen(expected) + id.size() + strlen(but_got) +
-                    instance->id.size());
+        msg.reserve(
+          strlen(expected) + id.size() +
+          strlen(but_got) + instance->id.size());
         msg += expected;
         msg += id;
         msg += but_got;
@@ -424,6 +528,5 @@ Json::Value serialize_sampling(const Sampling& sampling) {
 
 }  // namespace sampling
 }  // namespace mapried
-}  // namespace scapremai
 
 // File automatically generated by mapry. DO NOT EDIT OR APPEND!
